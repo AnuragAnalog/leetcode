d5bb9293460f20c0f8ab176b09e64f6f3d98cbbe
Peddi Anurag <32128318+AnuragAnalog@users.noreply.github.com>
2024-06-08 18:41:47 -0400

    Create tabulation.py
    AC

---
+++ problems/house-robber-ii/tabulation.py
@@ -0,0 +1,20 @@
+class Solution:
+    def robHouses(self, houses):
+        n = len(houses)
+        value = [0] * len(houses)
+        value[0] = houses[0]
+        value[1] = max(houses[0], houses[1])
+
+        for i in range(2, n):
+            value[i] = max(houses[i] + value[i - 2], value[i-1])
+
+        return value[-1]
+
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        elif len(nums) == 2:
+            return max(nums)
+
+        n = len(nums)
+        return max(self.robHouses(nums[1:]), self.robHouses(nums[:-1]))
