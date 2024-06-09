8e795dcdc22c28484862566ee4c7dc2f483f1cb4
Peddi Anurag <32128318+AnuragAnalog@users.noreply.github.com>
2024-06-08 21:27:54 -0400

    Create memorization.py
    TLE

---
+++ problems/ninjas-training/memorization.py
@@ -0,0 +1,25 @@
+from typing import *
+
+def f(ind, points, sel_index):
+    if ind == 0:
+        maxi = 0
+        for i in range(3):
+            if i != sel_index:
+                maxi = max(maxi, points[ind][i])
+
+        return maxi
+
+    maxi = 0
+    for i in range(3):
+        if i != sel_index:
+            maxi = max(maxi, points[ind][i] + f(ind - 1, points, i))
+
+    return maxi
+  
+def ninjaTraining(n: int, points: List[List[int]]) -> int:
+    # ll = list()
+    # Write your code here.
+    # for i in range(3):
+        # ll.append(f(n - 1, points, i))
+    # return max(ll)
+    return f(n - 1, points, 3)
