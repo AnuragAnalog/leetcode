de0787c659880f277be3f368e4f4fab054c0be33
Peddi Anurag <32128318+AnuragAnalog@users.noreply.github.com>
2024-06-09 21:08:44 -0400

    Create recursion.py
    TLE

---
+++ problems/subset-sum-equal-to-k/recursion.py
@@ -0,0 +1,22 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def include(arr, ind, n, k, curr_sum):
+    if ind < 0:
+        return False
+    elif curr_sum + arr[ind] == k:
+        return True
+    else:
+        not_take = include(arr, ind-1, n, k, curr_sum)
+        take = include(arr, ind - 1, n, k, curr_sum + arr[ind])
+
+    return not_take or take
+
+def subsetSumToK(n, k, arr):
+    curr_sum = 0
+    n = len(arr)
+    # Write your code here
+    # Return a boolean variable 'True' or 'False' denoting the answer
+    return include(arr, n - 1, n, k, curr_sum)
