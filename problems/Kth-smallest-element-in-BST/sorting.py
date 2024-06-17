e8b9bdbe45a97b46bc272719c68fe6bf6a3e8c70
Peddi Anurag <32128318+AnuragAnalog@users.noreply.github.com>
2024-06-17 16:21:30 -0400

    Create sorting.py
    AC

---
+++ problems/Kth-smallest-element-in-BST/sorting.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.values = list()
+
+    def inOrder(self, root):
+        if root is None:
+            return None
+        else:
+            self.inOrder(root.left)
+            self.values.append(root.val)
+            self.inOrder(root.right)
+
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        self.inOrder(root)
+
+        return self.values[k-1]
