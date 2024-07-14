4218b5b39023550da68dc73197293a29373d662b
Peddi Anurag <32128318+AnuragAnalog@users.noreply.github.com>
2024-07-14 16:37:52 -0400

    Create solution.py
    Python AC

---
+++ problems/9/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        rev_number = 0
+        ox = x
+
+        while x != 0:
+            rev_number = rev_number*10+x%10
+            x = int(x//10)
+
+        if ox == rev_number:
+            return True
+        else:
+            return False
