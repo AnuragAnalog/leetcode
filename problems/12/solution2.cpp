a6c5eb54d8c652dddd3487a249165a977dcae3dc
Peddi Anurag <32128318+AnuragAnalog@users.noreply.github.com>
2024-07-14 16:50:38 -0400

    Create solution2.cpp
    C++ AC2

---
+++ problems/12/solution2.cpp
@@ -0,0 +1,168 @@
+class Solution
+{
+public:
+    string intToRoman(int num)
+    {
+        // map <int, string>    encoding;
+        int        i = 0, j, q;
+        vector <int>    roman;
+        string roman_s;
+        
+        // encoding[1] = 'I';
+        // encoding[2] = 'V';
+        // encoding[3] = 'X';
+        // encoding[4] = 'L';
+        // encoding[5] = 'C';
+        // encoding[6] = 'D';
+        // encoding[7] = 'M';
+            
+        q = num / 1000;
+        for (j = 0; j < q; j++)
+        {
+            roman.push_back(7);
+            i = i + 1;
+        }
+        num = num - q * 1000;
+        
+        if (num/100 == 9)
+        {
+            roman.push_back(5);
+            roman.push_back(7);
+            i = i + 2;
+        }
+        else if (num/100 >= 4 && num/100 <= 8)
+        {
+            if (num/100 == 4)
+            {
+                roman.push_back(5);
+                roman.push_back(6);
+                i = i + 2;
+            }
+            else
+            {
+                roman.push_back(6);
+                i = i + 1;
+                for (j = 0; j < num/100-5; j++)
+                {
+                    roman.push_back(5);
+                    i = i + 1;
+                }
+            }
+        }
+        else
+        {
+            for (j = 0; j < num/100; j++)
+            {
+                roman.push_back(5);
+                i = i + 1;
+            }
+        }
+        
+        num = num - (num/100)*100;
+        
+        if (num/10 == 9)
+        {
+            roman.push_back(3);
+            roman.push_back(5);
+            i = i + 2;
+        }
+        else if (num/10 >= 4 && num/10 <= 8)
+        {
+            if (num/10 == 4)
+            {
+                roman.push_back(3);
+                roman.push_back(4);
+                i = i + 2;
+            }
+            else
+            {
+                roman.push_back(4);
+                i = i + 1;
+                for (j = 0; j < num/10-5; j++)
+                {
+                    roman.push_back(3);
+                    i = i + 1;
+                }
+            }
+        }
+        else
+        {
+            for (j = 0; j < num/10; j++)
+            {
+                roman.push_back(3);
+                i = i + 1;
+            }
+        }
+        
+        num = num - (num/10)*10;
+        
+        if (num == 9)
+        {
+            roman.push_back(1);
+            roman.push_back(3);
+            i = i + 2;
+        }
+        else if (num >= 4 && num <= 8)
+        {
+            if (num == 4)
+            {
+                roman.push_back(1);
+                roman.push_back(2);
+                i = i + 2;
+            }
+            else
+            {
+                roman.push_back(2);
+                i = i + 1;
+                for (j = 0; j < num-5; j++)
+                {
+                    roman.push_back(1);
+                    i = i + 1;
+                }
+            }
+        }
+        else
+        {
+            for (j = 0; j < num; j++)
+            {
+                roman.push_back(1);
+                i = i + 1;
+            }
+        }
+        
+        for (auto c: roman)
+        {
+            if (c == 1)
+            {
+                roman_s += 'I';
+            }
+            else if (c == 2)
+            {
+                roman_s += 'V';
+            }
+            else if (c == 3)
+            {
+                roman_s += 'X';
+            }
+            else if (c == 4)
+            {
+                roman_s += 'L';
+            }
+            else if (c == 5)
+            {
+                roman_s += 'C';
+            }
+            else if (c == 6)
+            {
+                roman_s += 'D';
+            }
+            else if (c == 7)
+            {
+                roman_s += 'M';
+            }
+            // roman_s += encoding[c];
+        }
+        
+        return roman_s;
+    }
+};
