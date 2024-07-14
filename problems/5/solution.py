33d17d06e046f34e4d493c2b7f95a6f07c9ed85d
Peddi Anurag <32128318+AnuragAnalog@users.noreply.github.com>
2024-07-14 16:34:27 -0400

    Create solution.py
    Python AC

---
+++ problems/5/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def palindrome(self, s) -> str:
+        return s == s[::-1]
+
+    def longestPalindrome(self, s: str) -> str:
+        maxi_str = ""
+        maxi = 0
+        n = len(s)
+
+        if n == 1:
+            return s
+
+        for i in range(n):
+            for j in range(i+1, n+1):
+                if self.palindrome(s[i:j]):
+                    if maxi < len(s[i:j]):
+                        maxi = len(s[i:j])
+                        maxi_str = s[i:j]
+
+        return maxi_str
