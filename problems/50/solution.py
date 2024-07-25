aa6003635e19fd837bbf42853e0b3e49c8984d4e
Peddi Anurag <32128318+AnuragAnalog@users.noreply.github.com>
2024-07-25 09:59:26 -0400

    Create solution.py
    AC

---
+++ problems/50/solution.py
@@ -0,0 +1,26 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        # Binary Exponential
+        final = x
+        neg = False
+
+        if n == 0:
+            return 1
+
+        if n < 0:
+            n = -1 * n
+            neg = True
+
+        bins = ""
+        while n != 0:
+            bins += str(n&1)
+            n = n >> 1
+
+        for dig in bins[::-1][1:]:
+            final *= final
+            if dig == "1":
+                final *= x
+
+        if neg:
+            return 1 / final
+        return final
