577a634413874b30a3c9c9d81072cde940019758
Anurag <32128318+AnuragAnalog@users.noreply.github.com>
2024-10-23 14:43:32 -0400

    Create bfs.py
    added bfs

---
+++ others/bipartite-graph/bfs.py
@@ -0,0 +1,24 @@
+class Solution:
+    def two_colors(self, n, adj, visited):
+        q = [n]
+        visited[n] = 0
+        
+        while q:
+            node = q.pop(0)
+            for n in adj[node]:
+                if visited[n] == -1:
+                    visited[n] = 1 - visited[node]
+                    q.append(n)
+                elif visited[n] == visited[node]:
+                    return False
+    
+        return True
+
+	def isBipartite(self, V, adj):
+	   visited = [-1] * V
+	    
+	   for v in range(V):
+	       if visited[v] == -1:
+	           if not self.two_colors(v, adj, visited):
+	               return False
+	   return True
