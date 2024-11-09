7c099a2f407b33198b7de053706cc51a54013aa5
Anurag <32128318+AnuragAnalog@users.noreply.github.com>
2024-11-09 17:14:13 -0500

    Create solution1.py

---
+++ problems/112/solution1.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def search(self, root, curr_sum):
+        if root is None:
+            return 0
+        if root.left is None and root.right is None:
+            if self.target_sum == curr_sum + root.val:
+                self.path_found = True
+        
+        self.search(root.left, curr_sum + root.val)
+        self.search(root.right, curr_sum + root.val)
+
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        self.path_found = False
+        self.target_sum = targetSum
+
+        self.search(root, 0)
+
+        return self.path_found
